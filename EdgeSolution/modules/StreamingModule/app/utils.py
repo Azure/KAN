20554a92429877932893a1eec4cdd8225add91a4
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2022-06-06 23:30:03 +0800

    move drawing function to utils

---
+++ EdgeSolution/modules/StreamingModule/app/utils.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+
+def insights_overlay(img, frame):
+
+    h, w, _ = img.shape
+
+    for object_meta in frame.insights_meta.objects_meta:
+        bbox = object_meta.bbox
+        p1 = int(bbox.l * w), int(bbox.t * h)
+        p2 = int((bbox.l+bbox.w) * w), int((bbox.t+bbox.h) * h)
+        color = (0, 0, 255)
+        thickness = 2
+
+        cv2.rectangle(img, p1, p2, color, thickness)
+
+        label = object_meta.label
+        fontScale = 1
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        org = p1[0], p1[1]-20
+        thickness = 2
+        color = (0, 0, 255)
+        cv2.putText(img, label, org, font,
+               fontScale, color, thickness, cv2.LINE_AA)

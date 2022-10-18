fcc00abd0085be00ee2f450f5c44c46b7a3e6e2a
ronpai <ronpai0919@gmail.com>
2022-10-18 18:12:46 +0800

    feat(voeedge): sync with latest symphony architecture

--- src/edge/EdgeSolution/modules/voeedge/app/main.py
+++ src/edge/EdgeSolution/modules/voeedge/app/main.py
@@ -17,7 +17,7 @@ from common.voe_model_config import ModelConfig
 from common.voe_ipc import PredictModule, StreamingModule
 from common.voe_status import StatusEnum
 
-#logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
 from predict_module import predict_module
 from streaming_module import streaming_module
@@ -32,9 +32,8 @@ STATUS_WAITING_SYMPHONY_AGENT = '1'
 STATUS_INITIALIZING_PREDICTMODULE = '2'
 STATUS_INITIALIZING_STREAMINGMODULE = '3'
 
+
 def process_skill(skill, skill_name):
-
-
 
     skill_spec = SkillSpec(**skill['spec'])
 
@@ -42,17 +41,15 @@ def process_skill(skill, skill_name):
     edges = []
     model_configs = []
 
-
     for skill_node in skill_spec.nodes:
-        
+
         logging.debug(skill_node)
-        
+
         if skill_node.type == 'model':
 
-            # skill_node.name is symphony's model name 
+            # skill_node.name is symphony's model name
             # we need to send a request to get more information from symphony model
-            # and then parse the model.type / subtype / project 
-
+            # and then parse the model.type / subtype / project
 
             # symphony_model & symphony_model_spec is the term from symphony
             # model_name, model_provider, model_executor, model_type is the term from ModelConfig
@@ -60,21 +57,22 @@ def process_skill(skill, skill_name):
             symphony_model_name = skill_node.name
             model_name = skill_node.name
             model = client.get_model(model_name)
-    
+
             symphony_model_spec = ModelSpec(**model['spec'])
             print('s -->', symphony_model_spec)
 
-            model_executor, model_type = symphony_model_spec.properties.model_subtype.split('.')
-            #FIXME 
-            if model_executor == 'intel': 
+            model_executor, model_type = symphony_model_spec.properties.model_subtype.split(
+                '.')
+            # FIXME
+            if model_executor == 'intel':
                 model_executor = 'openvino'
-                #FIXME
+                # FIXME
                 model_name = symphony_model_spec.properties.model_project
 
             model_provider = symphony_model_spec.properties.model_type
 
             #print('-->', model_executor, model_type)
-            #exit()
+            # exit()
             model_config = ModelConfig(
                 name=model_name,
                 provider=model_provider,
@@ -82,16 +80,16 @@ def process_skill(skill, skill_name):
                 type=model_type,
             )
 
-     
             if model_config.provider == 'customvision' and \
-                model_config.executor == 'customvision' and \
-                model_config.type == 'ObjectDetection':
-                
+                    model_config.executor == 'customvision' and \
+                    model_config.type == 'ObjectDetection':
+
                 while True:
                     try:
                         res = client.export_model(model_name)
                         if res is None or len(res) == 0:
-                            print(f'Model {model_name} is Not Ready to Export', flush=True)
+                            print(
+                                f'Model {model_name} is Not Ready to Export', flush=True)
                         elif len(res) > 0:
                             m = res[0]
                             if m['status'] == 'Exporting':
@@ -99,27 +97,26 @@ def process_skill(skill, skill_name):
                             elif m['status'] == 'Done':
                                 download_uri = m['downloadUri']
                                 break
-                                
+
                     except Exception as e:
                         print(e, flush=True)
-                        
+
                     time.sleep(3)
-                    
+
                 else:
-                    raise Exception('Unknown subtype {skill_node.sub_type} for customvision')
+                    raise Exception(
+                        'Unknown subtype {skill_node.sub_type} for customvision')
 
                 print('Got Download URI', download_uri)
                 model_config.download_uri = download_uri
 
             model_configs.append(model_config)
-                
 
             if model_type == 'ObjectDetection':
                 node_name = 'object_detection_model'
             elif model_type == 'Classification':
                 node_name = 'classification_model'
 
-            
             if skill_node.configurations is not None:
                 configurations = skill_node.configurations.copy()
             else:
@@ -127,7 +124,7 @@ def process_skill(skill, skill_name):
             configurations['model'] = model_name
             configurations['symphony_name'] = symphony_model_name
             configurations['provider'] = model_provider
-            
+
             node = Node(
                 id=skill_node.id,
                 type='model',
@@ -156,7 +153,6 @@ def process_skill(skill, skill_name):
 
         nodes.append(node)
 
-
     for skill_edge in skill_spec.edges:
         edge = Edge(
             source=skill_edge.source.node,
@@ -168,17 +164,14 @@ def process_skill(skill, skill_name):
 
     return cascade_config, model_configs
 
+
 if __name__ == '__main__':
 
-
-    
-    
     raw_aiskills = os.environ.get('AISKILLS')
     instance_name = os.environ.get('INSTANCE')
 
     print(raw_aiskills)
 
-    
     while True:
         try:
             instance = client.get_instance(instance_name)
@@ -195,43 +188,47 @@ if __name__ == '__main__':
     print(instance_spec)
 
     # update status
-    client.post_instance_status(instance_name, STATUS_WAITING_SYMPHONY_AGENT, "waiting symphony agent")
-
+    client.post_instance_status(
+        instance_name, STATUS_WAITING_SYMPHONY_AGENT, "waiting symphony agent")
 
     print('ok', flush=True)
-    
+
     cascade_configs = []
     model_configs = []
 
-    for skill_with_alias in json.loads(raw_aiskills):
-        skill_name, skill_alias = skill_with_alias.split(" as ")
+    for pipeline in instance['spec']['pipelines']:
+        skill_name, skill_alias = pipeline['skill'], pipeline['name']
         #skill = client.get_skill(skill_name)
-        skill = client.get_skill_with_instance_name_and_alias(skill_name, instance_name, skill_alias)
-        print(f'skill_name: {skill_name}, skill_alias: {skill_alias}', flush=True)
+        skill = client.get_skill_with_instance_name_and_alias(
+            skill_name, instance_name, skill_alias)
+        print(
+            f'skill_name: {skill_name}, skill_alias: {skill_alias}', flush=True)
         print(skill, flush=True)
-        new_cascade_config, new_model_configs = process_skill(skill, skill_name)
+        new_cascade_config, new_model_configs = process_skill(
+            skill, skill_name)
 
         cascade_configs.append(new_cascade_config)
         model_configs += new_model_configs
 
- 
-    streamingmodule_setting = StreamingModule.Setting(cascade_configs=cascade_configs)
+    streamingmodule_setting = StreamingModule.Setting(
+        cascade_configs=cascade_configs)
     predictmodule_setting = PredictModule.Setting(model_configs=model_configs)
-    
-    
+
     print('Initializing predict module', flush=True)
-    client.post_instance_status(instance_name, STATUS_INITIALIZING_PREDICTMODULE, "initialzing predict module")
+    client.post_instance_status(
+        instance_name, STATUS_INITIALIZING_PREDICTMODULE, "initialzing predict module")
     predict_module.set(predictmodule_setting)
 
     print('Initializing streaming module', flush=True)
-    client.post_instance_status(instance_name, STATUS_INITIALIZING_STREAMINGMODULE, "initialzing streaming module")
+    client.post_instance_status(
+        instance_name, STATUS_INITIALIZING_STREAMINGMODULE, "initialzing streaming module")
     streaming_module.start(streamingmodule_setting)
 
     print('Running', flush=True)
     client.post_instance_status(instance_name, STATUS_RUNNING, "running")
 
     streaming_module.join()
-    
+
     """
     while True:
         try:
@@ -287,5 +284,3 @@ if __name__ == '__main__':
    
         time.sleep(3)
     """
-
-

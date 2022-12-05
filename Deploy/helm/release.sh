61c353288eb4ad1af1ff455f4c6163f620ed0259
weitongxuan <hellosimon27@gmail.com>
2022-12-05 11:19:53 +0800

    update version.txt

--- Deploy/helm/release.sh
+++ Deploy/helm/release.sh
@@ -1,5 +1,5 @@
 ACR_NAME=$1
-VERSION=$(cat ../src/portal/modules/webmodule/version.txt)
+VERSION=$(cat ../src/portal/modules/webmodule/version.txt)-amd64
 helm package --version $VERSION --app-version $VERSION voe
 helm push voe-$VERSION.tgz oci://$ACR_NAME.azurecr.io/helm
 if [ "$?" -ne 0 ]

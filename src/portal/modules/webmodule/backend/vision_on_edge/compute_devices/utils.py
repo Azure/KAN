4ccc9dc3587a474be3a0a6a7a47a45723a682a37
ronpai <ronpai0919@gmail.com>
2022-12-22 16:36:14 +0800

    feat(portal): add non azure condition

--- src/portal/modules/webmodule/backend/vision_on_edge/compute_devices/utils.py
+++ src/portal/modules/webmodule/backend/vision_on_edge/compute_devices/utils.py
@@ -14,15 +14,21 @@ def get_credential():
     tenant_id = os.getenv('TENANT_ID')
     client_id = os.getenv('CLIENT_ID')
     client_secret = os.getenv('CLIENT_SECRET')
-    return ClientSecretCredential(
-        tenant_id=tenant_id,
-        client_id=client_id,
-        client_secret=client_secret
-    )
+    if tenant_id and client_id and client_secret:
+        return ClientSecretCredential(
+            tenant_id=tenant_id,
+            client_id=client_id,
+            client_secret=client_secret
+        )
+    else:
+        return ""
 
 
 def list_iothub():
     credential = get_credential()
+    if not credential:
+        return []
+
     subscription_id = os.getenv('SUBSCRIPTION_ID')
     resource_client = ResourceManagementClient(credential, subscription_id)
     rs = resource_client.resources.list()
@@ -35,7 +41,9 @@ def list_iothub():
 
 
 def list_devices(iothub_name):
-    # credential = get_credential()
+    credential = get_credential()
+    if not credential:
+        return []
     # iothub_url = f"{iothub_name}.azure-devices.net"
     # iot = IoTHubRegistryManager.from_token_credential(
     #     url=iothub_url, token_credential=credential)

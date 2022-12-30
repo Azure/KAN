2263040104f6f2bdeecb6f9fe743b103ee20e473
ronpai <ronpai0919@gmail.com>
2022-12-30 15:26:41 +0800

    feat(grpc_proto): add README, license

---
+++ src/grpc_proto/README.md
@@ -0,0 +1,19 @@
+# grpc_proto
+
+An easy grpc server that forwards the frame data.
+
+
+
+## Build 
+
+Build the grpc_proto container using docker:
+```
+    docker build -t {YOUR_CONTAINER_REGISTRY}/grpc_proto:latest .
+```
+
+## Push
+
+Upload the grpc_proto container to container registry
+```
+    docker push {YOUR_CONTAINER_REGISTRY}/grpc_proto:latest
+```
--- src/grpc_proto/custom_node_server.py
+++ src/grpc_proto/custom_node_server.py
@@ -1,7 +1,11 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 import grpc
 from concurrent import futures
 import custom_node_pb2
 import custom_node_pb2_grpc
+
 
 class CustomNode:
 
@@ -15,20 +19,18 @@ class CustomNode:
         raise NotImplementedError
 
 
-
-
 class CustomNodeServer:
     def __init__(self, port, node):
         self.port = port
         self.server = grpc.server(futures.ThreadPoolExecutor())
-        custom_node_pb2_grpc.add_CustomNodeHandlerServicer_to_server(node, self.server)
+        custom_node_pb2_grpc.add_CustomNodeHandlerServicer_to_server(
+            node, self.server)
         self.server.add_insecure_port(f'[::]:{port}')
 
     def serve(self):
         self.server.start()
         print(f'Server started, listening on {self.port}', flush=True)
         self.server.wait_for_termination()
-
 
     def close(self):
         self.channel.close()
@@ -38,22 +40,21 @@ if __name__ == '__main__':
 
     class FakeDetection(CustomNode):
 
-
         def Handshake(self, request: custom_node_pb2.HandshakeRequest, context) -> custom_node_pb2.HandshakeResponse:
             return custom_node_pb2.HandshakeResponse(
-                ack=request.seq, 
+                ack=request.seq,
                 image_type=custom_node_pb2.ImageType.IMAGE_TYPE_NUMPY,
                 process_request_type=custom_node_pb2.ProcessRequestType.FRAME_WITH_IMAGE,
                 process_response_type=custom_node_pb2.ProcessResponseType.INSIGHTS_META_ONLY,
             )
 
-
         def Process(self, request: custom_node_pb2.ProcessRequest, context) -> custom_node_pb2.ProcessResponse:
             print('processing ...')
             insights_meta = custom_node_pb2.InsightsMeta(
                 objects_meta=[
-                    custom_node_pb2.ObjectMeta(    
-                        timestamp=custom_node_pb2.Timestamp(seconds=10, nanos=100),
+                    custom_node_pb2.ObjectMeta(
+                        timestamp=custom_node_pb2.Timestamp(
+                            seconds=10, nanos=100),
                         label='car',
                         confidence=0.9,
                         inference_id='inference_1',
@@ -65,6 +66,5 @@ if __name__ == '__main__':
 
             return custom_node_pb2.ProcessResponse(ack=request.seq, insights_meta=insights_meta)
 
-    
     server = CustomNodeServer(6677, FakeDetection())
     server.serve()

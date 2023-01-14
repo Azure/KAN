861a935a67309e0e63c4bc05438dbfbda8e7f7e2
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2023-01-15 03:35:18 +0800

    fix symphonyai for nonazure deployment

--- src/edge/EdgeSolution/modules/common/common/azure_utils.py
+++ src/edge/EdgeSolution/modules/common/common/azure_utils.py
@@ -5,7 +5,13 @@ from azure.storage.blob import BlobServiceClient
 
 from common.env import BLOB_STORAGE_CONNECTION_STRING, BLOB_STORAGE_CONTAINER
 
-blob_storage_client = BlobServiceClient.from_connection_string(BLOB_STORAGE_CONNECTION_STRING)
+try:
+    blob_storage_client = BlobServiceClient.from_connection_string(BLOB_STORAGE_CONNECTION_STRING)
+except:
+    blob_storage_client = None
 
 def get_blob_client(filename):
+
+    if blob_storage_client is None: return None
+
     return blob_storage_client.get_blob_client(BLOB_STORAGE_CONTAINER, filename)
